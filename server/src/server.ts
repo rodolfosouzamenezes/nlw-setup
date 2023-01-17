f257ac9f18a7a3c9a936bad2f3004c2fc814a11c
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2023-01-17 12:50:25 -0300

    feat(server): added cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+import cors from "@fastify/cors";
 import fastify from "fastify";
 
 const port = 3333;
 const app = fastify();
+const prisma = new PrismaClient()
 
-app.get('/', () => {
-  return 'Hello'
+app.register(cors)
+
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: 'b'
+      }
+    }
+  })
+  return habits
 })
 
 app.listen({ port }).then(() => {
