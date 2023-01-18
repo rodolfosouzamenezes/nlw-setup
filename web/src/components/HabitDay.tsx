504279f9330e4453c413741dfda63f4b6e018261
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2023-01-18 14:08:11 -0300

    feat(web): summary date filled

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -4,6 +4,6 @@ interface HabitDayProps {
 
 export function HabitDay({}: HabitDayProps) {
   return (
-    <div className="h-10 w-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+    <div className="h-10 w-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg cursor-pointer"></div>
   )
 }
--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,14 +1,11 @@
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning"
 import { HabitDay } from "./HabitDay"
 
-const weekdays = [
-  'D',
-  'S',
-  'T',
-  'Q',
-  'Q',
-  'S',
-  'S',
-]
+const weekdays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+const summaryDates = generateDatesFromYearBeginning()
+
+const minimumSummaryDatesSize = 18 * 7;
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
 export function SummaryTable() {
   return (
@@ -23,7 +20,17 @@ export function SummaryTable() {
         })}
       </div>
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        <HabitDay />
+        {summaryDates.map((date) => {
+          return (
+            <HabitDay key={date.toString()} />
+          )
+        })}
+
+        {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, index) => {
+          return (
+            <div key={index} className="h-10 w-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40" />
+          )
+        })}
       </div>
     </div>
   )
