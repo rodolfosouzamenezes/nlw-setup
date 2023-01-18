ba6698f98234778ca723eaaf9467054c6a53b9c3
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2023-01-18 09:43:47 -0300

    feat(server): create habit route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,36 @@
+import dayjs from "dayjs"
 import { FastifyInstance } from "fastify"
+import { z } from "zod"
 import { prisma } from "./lib/prisma"
 
 export async function appRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
+  app.post('/habits', async (request) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(
+        z.number().min(0).max(6)
+      )
+    })
+    const { title, weekDays } = createHabitBody.parse(request.body)
+
+    const today = dayjs().startOf('day').toDate()
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: today,
+        weekDays: {
+          create: weekDays.map(weeKDay => {
+            return {
+              week_day: weeKDay
+            }
+          })
+        }
+      }
+    })
+  })
+
+  app.get('/habits', async () => {
     const habits = await prisma.habit.findMany()
 
     return habits
