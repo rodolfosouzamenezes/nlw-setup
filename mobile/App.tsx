365e36f2e26723424c1fbf65b980ba869341922b
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2023-01-18 14:47:00 -0300

    feat(mobile): nativewind configured

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,4 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import {
   useFonts,
   Inter_400Regular,
@@ -8,6 +7,7 @@ import {
   Inter_800ExtraBold
 } from '@expo-google-fonts/inter';
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -24,22 +24,10 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Habits</Text>
+    <>
+      <Home />
       <StatusBar style="light" translucent />
-    </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
