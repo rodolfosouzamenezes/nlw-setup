390ea6ab07f7a2dfe47db2e58a6ea1fb23ca9dd8
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2023-01-19 19:27:05 -0300

    feat(mobile): back button created

--- mobile/src/screens/NewHabit.tsx
+++ mobile/src/screens/NewHabit.tsx
@@ -1,9 +1,12 @@
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
+import { BackButton } from "../components/BackButton";
 
 export function NewHabit() {
   return (
     <View className="flex-1 bg-background px-8 pt-16">
-
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <BackButton />
+      </ScrollView>
     </View>
   )
 }
