70e3ae62721a8b4c0e6e9d9ed2a20df5305c9811
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2023-01-19 06:01:47 -0300

    feat(server): toggle habit route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -37,7 +37,7 @@ export async function appRoutes(app: FastifyInstance) {
 
     const { date } = getDayParams.parse(request.query)
     const parsedDate = dayjs(date).startOf('day')
-    const weekDay = dayjs(parsedDate).get('day')    
+    const weekDay = dayjs(parsedDate).get('day')
 
     const possibleHabits = await prisma.habit.findMany({
       where: {
@@ -69,5 +69,56 @@ export async function appRoutes(app: FastifyInstance) {
       possibleHabits,
       completedHabits
     }
+  })
+
+  app.patch('/habits/:id/toggle', async (request) => {
+    const toggleHabitsParams = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = toggleHabitsParams.parse(request.params)
+
+    const today = dayjs().startOf('day').toDate()
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today
+      }
+    })
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today
+        }
+      })
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id
+        }
+      }
+    })
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        }
+      })
+
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id
+        }
+      })
+    }
+
+
   })
 }
