6306519d0721cb5fd720716ddb5786147c03d279
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2023-01-21 18:25:49 -0300

    style(mobile): checkbox animated

--- mobile/src/components/Checkbox.tsx
+++ mobile/src/components/Checkbox.tsx
@@ -1,6 +1,7 @@
 import { Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
 import { Feather } from '@expo/vector-icons'
 import colors from 'tailwindcss/colors'
+import Animated, { RollInRight, ZoomIn, ZoomOut } from "react-native-reanimated";
 
 interface CheckboxProps extends TouchableOpacityProps {
   checked?: boolean;
@@ -17,13 +18,17 @@ export function Checkbox({ checked = false, title, ...rest }: CheckboxProps) {
       {
         checked
           ?
-          <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
+          <Animated.View
+            entering={ZoomIn}
+            exiting={ZoomOut}
+            className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center"
+          >
             <Feather
               name="check"
               color={colors.white}
               size={20}
             />
-          </View>
+          </Animated.View>
           :
           <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
       }
